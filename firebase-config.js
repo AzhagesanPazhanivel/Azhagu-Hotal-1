603699579d51ac221d9dcc390bf8d7c4ade76903
AzhagesanPazhanivel <villageinfotechclient@gmail.com>
2026-04-20 19:56:00 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// ========== FIREBASE CONFIGURATION ==========
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBLrk_nmC2V0NvRKfhHdFzBi647xiuF1y0",
+    authDomain: "azhagu-hotel.firebaseapp.com",
+    projectId: "azhagu-hotel",
+    storageBucket: "azhagu-hotel.firebasestorage.app",
+    messagingSenderId: "357401937469",
+    appId: "1:357401937469:web:a7a55fc0e59a5059660c54"
+};
+
+firebase.initializeApp(FIREBASE_CONFIG);
+const db = firebase.firestore();
