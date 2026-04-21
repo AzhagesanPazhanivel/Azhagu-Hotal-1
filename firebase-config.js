3e82c4e50e4da73b48839225554c4443a066ea15
AzhagesanPazhanivel <villageinfotechclient@gmail.com>
2026-04-21 13:44:29 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,13 @@
-const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyBLrk_nmC2V0NvRKfhHdFzBi647xiuF1y0",
-    authDomain: "azhagu-hotel.firebaseapp.com",
-    projectId: "azhagu-hotel",
-    storageBucket: "azhagu-hotel.firebasestorage.app",
-    messagingSenderId: "357401937469",
-    appId: "1:357401937469:web:a7a55fc0e59a5059660c54"
+// Firebase Configuration - Azhagu Unavagam
+const firebaseConfig = {
+    apiKey: "AIzaSyAxmp51J6OrqxGcqm_pkrbjN5PMb0DcDvk",
+    authDomain: "azhagu-unavagam.firebaseapp.com",
+    projectId: "azhagu-unavagam",
+    storageBucket: "azhagu-unavagam.firebasestorage.app",
+    messagingSenderId: "22877925035",
+    appId: "1:22877925035:web:2fc107913da907a3cf91ee"
 };
 
-firebase.initializeApp(FIREBASE_CONFIG);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
